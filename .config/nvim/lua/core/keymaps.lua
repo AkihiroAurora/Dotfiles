b7535abd5d9e9428ad09f4cf18c67dd5dee7eb5e
AkihiroAurora <AkihiroAurora@proton.me>
2024-10-25 17:18:51 +0330

    commit

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -27,22 +27,4 @@ vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
 vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
 
 -- open Lazy gui
-vim.keymap.set('n', '<leader>l', '<cmd> Lazy <CR>', { noremap = true, silent = true, desc = 'Open Lazy menu' })
-
--- Window management
-vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- split window vertically
-vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
-vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
-vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
-
--- Resize with arrows
-vim.keymap.set('n', '<S-j>', ':resize -2<CR>', opts)
-vim.keymap.set('n', '<S-k>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<S-h>', ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<S-l>', ':vertical resize +2<CR>', opts)
-
--- Navigate between splits
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', opts)
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', opts)
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', opts)
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', opts)
+vim.keymap.set('n', '<leader>L', '<cmd> Lazy <CR>', { noremap = true, silent = true, desc = 'Open Lazy menu' })
--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,26 +1,4 @@
 return {
-  -- Toggle terminal
-  {
-    { 'akinsho/toggleterm.nvim', version = '*', config = true },
-
-    vim.keymap.set('n', '<leader>tt', '<cmd> ToggleTerm <CR>', { desc = '[T]oggle [T]erminal' }),
-  },
-
-  -- Detect tabstop and shiftwidth automatically
-  {
-    'tpope/vim-sleuth',
-  },
-
-  -- Powerful Git integration for Vim
-  {
-    'tpope/vim-fugitive',
-  },
-
-  -- GitHub integration for vim-fugitive
-  {
-    'tpope/vim-rhubarb',
-  },
-
   -- Hints keybinds
   {
     'folke/which-key.nvim',
